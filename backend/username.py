cf25498378ae8ef29ca47d2ee325eb11da9e8a48
Druhin Bhowal <druhin.bhowal@outlook.com>
2020-07-23 15:47:16 -0700

    foo

---
+++ backend/username.py
@@ -0,0 +1,19 @@
+import json
+import os
+import sqlite3
+
+# Third-party libraries
+from flask import Flask, redirect, request, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+# Internal imports
+from username import User
+from db import init_db_command
